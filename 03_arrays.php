28f72021a1404753f1a51fe5054000453e3b3ff6
TzivyaAleph <91893941+TzivyaAleph@users.noreply.github.com>
2024-04-01 15:28:32 +0300

    Add files via upload

---
+++ 03_arrays.php
@@ -0,0 +1,15 @@
+<?php 
+    $numbers = [1,44,55];
+    $fruits = array('appple', 'orange');
+
+    print_r($numbers);
+
+    echo $fruits[1];
+
+    $colors = [5=> 'red', 4=>'blue'];
+
+    echo $colors[5];
+
+    $people = [['first_name'=>'brad', 'last_name'=> 'Rot', 'email'=>'tzivya@gmail.com'],['first_name'=>'John', 'last_name'=> 'Tucker', 'email'=>'matmets@gmail.com']];
+    print_r($people[1]);
+
